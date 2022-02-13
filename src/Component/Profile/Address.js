7cacb2c5f5a1119b4fb2bfba11d240fb461bd5e3
WaelBeldi <waelbeldi1990@gmail.com>
2022-02-13 22:52:56 +0100

    react components checkpoint

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+  return (
+    <div>
+        <h3>Address: Nabeul, Tunisie</h3>
+    </div>
+  )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import profilephoto from './ProfilePhoto.PNG'
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+        <img src={profilephoto} alt="ProfilePhoto" />
+    </div>
+  )
+}
+
+export default ProfilePhoto
